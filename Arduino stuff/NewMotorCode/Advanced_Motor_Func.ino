a0ad6dbd9731612f21326d9f51ad71be20430232
gkallberg <82403120+gkallberg@users.noreply.github.com>
2021-09-24 11:37:30 -0400

    added comments
    needed to do something and didn't have time to be productive so I added some comments to the code to clarify stuff

--- Arduino stuff/NewMotorCode/Advanced_Motor_Func.ino
+++ Arduino stuff/NewMotorCode/Advanced_Motor_Func.ino
@@ -1,25 +1,24 @@
-void test(){
+void test(){  //runs a serise of motor comands to make sure the motors are working and calabrated
   single_test();
   Forward(1000);
   Back(1000);
   Left(1000);
   Right(1000);
 }
-int S=0;
-void dontFall(){
+int S=0;  //variable used to trigger stop command once
+void dontFall(){  //code to check if going over a clif and correct
   int currentRead=analogRead(cliff);
-  
   delay(25);
   int lastRead=currentRead;
   currentRead=analogRead(cliff);
-  if (abs(lastRead-currentRead)>100 && S==0){
+  if (abs(lastRead-currentRead)>100 && S==0){ //check the last two readings and if there is a dramatic change stop going forward and initiate anti-falling mechanisms
     Stop();
     Serial.println(analogRead(cliff));
     Serial.println("stop");
     S=1;
     
   } 
-  if (S==1){
+  if (S==1){//the antifalling mechanism is currently just going backwards for half a seccond
     Back(500);
     S=2;    
   }
