2dd31d20366ec27c26412e8b76c61ef2a079b2c6
NerdiCat <chunt22@medford.k12.ma.us>
2021-09-28 13:20:06 -0400

    calibrating and troubleshooting

--- Arduino stuff/NewMotorCode/Advanced_Motor_Func.ino
+++ Arduino stuff/NewMotorCode/Advanced_Motor_Func.ino
@@ -8,10 +8,10 @@ void test(){  //runs a serise of motor comands to make sure the motors are worki
 int S=0;  //variable used to trigger stop command once
 void dontFall(){  //code to check if going over a clif and correct
   int currentRead=analogRead(cliff);
-  delay(25);
+  delay(50);
   int lastRead=currentRead;
   currentRead=analogRead(cliff);
-  if (abs(lastRead-currentRead)>100 && S==0){ //check the last two readings and if there is a dramatic change stop going forward and initiate anti-falling mechanisms
+  if (abs(lastRead-currentRead)>400 && S==0){ //check the last two readings and if there is a dramatic change stop going forward and initiate anti-falling mechanisms
     Stop();
     Serial.println(analogRead(cliff));
     Serial.println("stop");
